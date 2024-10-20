a7278f136e94ee6e857308b0f19ef0166dcd3029
John Geiger <75145269+jcoder1911@users.noreply.github.com>
2024-10-20 11:11:08 -0400

    getRandomQuote working
    getRandomQuote working but not printQuote yet

--- js/script.js
+++ js/script.js
@@ -10,7 +10,42 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
+let quotes = [
+  {
+    quote: "Every great developer you know got there by solving problems they were unqualified to solve until they actually did it.",
+    source: "Patrick McKenzie",
+    citation: "Twitter",
+    year: "2016"
+  },
+  {
+    quote: "In the midst of Chaos, there is also opportunity.",
+    source: "Sun Tzu",
+    citation: "The Art of War",
+    year: "5th Century BC"
+  },
+  {
+    quote: "The greatest victory is that wich requires no battle.",
+    source: "Sun Tzu",
+    citation: "The Art of War",
+    year: "5th Century BC"
+  },
+  {
+    quote: "Don't follow your dreams, chase them.",
+    source: "Anonymous"
+  },
+  {
+    quote: "You must be the change you wish to see in the world",
+    Source: "Mahatma Gandhi",
+    citation: "AZ Quotes"
+  },
+  {
+    quote: "Well done is better than well said.",
+    source: "Benjamin Franklin",
+    citation: "AZ Quotes"
+  }
 
+
+];
 
 
 /***
@@ -18,16 +53,42 @@ project 1 - A Random Quote Generator
 ***/
 
 
+function getRandomQuote (arr) {
+  const randomNum = Math.floor(Math.random() * arr.length);
+  return quotes[randomNum];
+};
+
+console.log(getRandomQuote(quotes));
+console.log(getRandomQuote(quotes.source));
 
 /***
  * `printQuote` function
 ***/
+function printQuote() {
+  let quote = `${getRandomQuote(quotes)}`;
+  let html = `
+    <p class= "quote" > ${quote.quote} </p>
+    <p class= "source" > ${quote.source}
+      if ( ${quote.citation} ) {
+        html += <span class = "citation"> ${quote.citation}; </span>
+      } 
+      if ( ${quote.year} ) {
+        html += <span class = "year"> ${quote.year} </span>
+      }
+    </p>
+  `
+  return html;
+}
 
+;
 
+document.getElementById('quote-box').innerHTML = printQuote();
 
+console.log(printQuote())
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
 
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
+//document.getElementById('load-quote').addEventListener("click", printQuote, false);
+
